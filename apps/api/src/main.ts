950258d3b388678cd7ed1aeae050f3804d61c9b5
zarif-tajwar <zariftjwr.catalyst@gmail.com>
2024-10-17 13:05:38 +0600

    chore(api): enable cors with the frontend website url

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,23 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { getEnvValue } from './config/env.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  const WEBSITE_URL = getEnvValue(configService, 'WEBSITE_URL');
+  const PORT = getEnvValue(configService, 'PORT') || 3000;
+
+  app.enableCors({
+    origin: [WEBSITE_URL],
+    credentials: true,
+  });
+
+  await app.listen(PORT);
+
+  console.log(`🚀 API Server is Listening at http://localhost:${PORT} 🚀`);
 }
 bootstrap();
